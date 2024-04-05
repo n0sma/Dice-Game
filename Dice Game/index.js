34c2592eae9e5f82c5609b12246e0622c85ec07c
n0sma <118612144+n0sma@users.noreply.github.com>
2024-04-05 12:53:26 +0300

    Add files via upload

---
+++ Dice Game/index.js
@@ -0,0 +1,31 @@
+var randomNumber1=Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2=Math.floor(Math.random()*6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "🚩 PLayer 1 Wins!";
+  
+}
+
+else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "PLayer 2 Wins! 🚩";
+  
+}
+else if (randomNumber1 === randomNumber2){
+  document.querySelector("h1").innerHTML = "Draw";
+  
+}
